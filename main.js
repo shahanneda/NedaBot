74b77c9e32a08930f4fe576cf6224afeb5aad0d0
Shahan Neda <shahan.neda@gmail.com>
2020-02-16 18:38:57 -0800

    redid system and refactored

--- main.js
+++ main.js
@@ -31,34 +31,20 @@ function handlePingPong(message){
 
 function handleHiIAm(message){
         let formatedMsg = message.content.toLowerCase();
-        let messageArray = formatedMsg.split(" ");
         
         let triggerPhrases = ["im", "i'm", "i am", "i am a", "im a"];
 
         triggerPhrases.map(function (phrase, index){
-                if( messageArray.indexOf("im") != -1){
-                        messageArray = messageArray.slice(messageArray.indexOf("im"), messageArray.length);
-                        console.log(messageArray);
+                if(formatedMsg.indexOf(phrase) != -1){
+                        formatedMsg = formatedMsg.substr(formatedMsg.indexOf(phrase) + phrase.length+1, formatedMsg.length);
+                        rname = formatedMsg.split(" ");
+                        sendHiMessage(message, rname[0]);
                 }
         });
 
+        let messageArray = formatedMsg.split(" ");
+
         let name  = "";
-        if(messageArray[0] == 'im' && messageArray[1] == "a"){
-                name = messageArray[2];
-                sendHiMessage(message, name);
-        }else
-        if(messageArray[0] ==  'im' || messageArray[0] == "i'm"){
-                name = messageArray[1];   
-                sendHiMessage(message, name);
-        }else
-        if(messageArray[0] == 'i' && messageArray[1] == "am" && messageArray[2] == "a"){
-                name = messageArray[3];
-                sendHiMessage(message, name);
-        }else
-        if(messageArray[0] == 'i' && messageArray[1] == "am"){
-                name = messageArray[2];
-                sendHiMessage(message, name);
-        }
 }
 function sendHiMessage(messageObject, name){
         name = setCharAt(name, 0, name.charAt(0).toString().toUpperCase());
